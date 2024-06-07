332e9c1d8f98e10f0efab092dbc9205a70e79680
Mick Letofsky <mickl@knowledgeservices.com>
2024-06-07 08:58:48 -0600

    Enhance coffee menu with ability to add menu item

--- shawskyRoasters/main.go
+++ shawskyRoasters/main.go
@@ -12,6 +12,8 @@ var userInput = bufio.NewReader(os.Stdin)
 
 func main() {
 
+	var storeMenu = menu.ImportMenu()
+
 loop:
 	for {
 		fmt.Println("Welcome to Shawsky Roasters! Please select an option to continue...")
@@ -22,15 +24,37 @@ loop:
 
 		switch strings.TrimSpace(choice) {
 		case "1":
-			menu.ImportMenu()
+			menu.PrintMenu(&storeMenu)
+		case "2":
+			name, category, prices := ReadMenuItem()
+			err := menu.AddMenuItem(&storeMenu.MenuItems, name, category, prices)
+			if err != nil {
+				fmt.Println(err)
+				fmt.Println("Unable to create new menu item. Please try again.")
+				fmt.Println()
+			}
 		case "q":
-			break loop
 		case "Q":
 			break loop
 		default:
 			fmt.Println("Unknown option... Please try again")
 		}
-
 	}
+}
 
+func ReadMenuItem() (string, string, []string) {
+	fmt.Println("Please enter the category of the new item")
+	category, _ := userInput.ReadString('\n')
+	category = strings.TrimSpace(category)
+
+	fmt.Println("Please enter the name of the new item")
+	name, _ := userInput.ReadString('\n')
+	name = strings.TrimSpace(name)
+
+	fmt.Println("Please enter the price of a small, medium, and large item delimited by pipes")
+	prices, _ := userInput.ReadString('\n')
+	prices = strings.TrimSpace(prices)
+	priceList := strings.Split(prices, "|")
+
+	return name, category, priceList
 }
--- shawskyRoasters/menu/menu.go
+++ shawskyRoasters/menu/menu.go
@@ -3,7 +3,10 @@ package menu
 import (
 	"embed"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 type Price struct {
@@ -25,17 +28,55 @@ type Menu struct {
 //go:embed menu.json
 var fileContents embed.FS
 
-func ImportMenu() {
+func ImportMenu() Menu {
 	config, _ := fileContents.ReadFile("menu.json")
 
-	var menu []MenuItem
+	var menu Menu
+	menu.MenuItems = []MenuItem{}
 
-	if err := json.Unmarshal(config, &menu); err != nil {
+	if err := json.Unmarshal(config, &menu.MenuItems); err != nil {
 		fmt.Printf("failed to unmarshal json file, error: %v", err)
-		return
+		return menu
 	}
-	for _, item := range menu {
+	return menu
+}
+
+func PrintMenu(input *Menu) {
+	fmt.Println()
+	for _, item := range input.MenuItems {
 		fmt.Printf("Name: %s, Category: %s \n", item.Name, item.Category)
 		fmt.Printf("\tPricing => Small: $%.2f    Medium: $%.2f    Large: $%.2f  \n", item.Prices.Small, item.Prices.Medium, item.Prices.Large)
+		fmt.Println()
 	}
+}
+
+func AddMenuItem(items *[]MenuItem, name string, category string, prices []string) error {
+	for _, item := range *items {
+		if item.Name == name {
+			return errors.New("The menu item already exists; please try again")
+		}
+	}
+
+	if len(prices) != 3 {
+		return errors.New("Unable to detect three distinct prices for the new menu item")
+	}
+
+	smallPrice, e := strconv.ParseFloat(strings.TrimSpace(prices[0]), 64)
+	if e != nil {
+		return errors.New("Unable to parse the small price provided")
+	}
+	mediumPrice, e := strconv.ParseFloat(strings.TrimSpace(prices[1]), 64)
+	if e != nil {
+		return errors.New("Unable to parse the medium price provided")
+	}
+
+	largePrice, e := strconv.ParseFloat(strings.TrimSpace(prices[2]), 64)
+	if e != nil {
+		return errors.New("Unable to parse the medium price provided")
+	}
+
+	newItem := MenuItem{Name: name, Category: category, Prices: Price{Small: smallPrice, Medium: mediumPrice, Large: largePrice}}
+
+	*items = append(*items, newItem)
+	return nil
 }

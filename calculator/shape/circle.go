dfaf0b77c39bf273c531f200cec64e9621e2be39
Mick Letofsky <mickl@knowledgeservices.com>
2024-08-19 16:29:43 -0600

    Fun with table (struct) driven testing & rounding circle data

--- calculator/shape/circle.go
+++ calculator/shape/circle.go
@@ -6,12 +6,22 @@ type Circle struct {
 	radius float64
 }
 
+const floatPrecision = 2
+
 func (c Circle) Area() float64 {
-	return math.Pi * math.Pow(c.radius, 2)
+	result := math.Pi * math.Pow(c.radius, 2)
+	return roundFloatUp(result, floatPrecision)
 }
 
 func (c Circle) Perimeter() float64 {
-	return 2 * math.Pi * c.radius
+	result := 2 * math.Pi * c.radius
+	return roundFloatUp(result, floatPrecision)
+}
+
+func roundFloatUp(value float64, precision uint) float64 {
+	ratio := math.Pow(10, float64(precision))
+	return math.Round(value*ratio) / ratio
+
 }
 
 func ConstructCircle(radius float64) Circle {
